e9ec54a0c630a1e2a43e9295c42a89ccd0015c4a
Erik Demitz-Helin <2969909+dodslaser@users.noreply.github.com>
2023-03-16 18:13:33 +0100

    Use mixins

--- src/data.py
+++ src/data.py
@@ -3,7 +3,7 @@
 from collections import UserDict, UserList
 from functools import reduce
 from pathlib import Path
-from typing import Any, Callable, Hashable, Mapping, Optional, Sequence, TypeVar
+from typing import Any, Callable, Hashable, Mapping, Optional, Sequence, TypeVar, get_args, get_origin
 
 from yaml import safe_load
 
@@ -60,11 +60,16 @@ class Sample(Container):
 
     def __init__(self, /, id, fastq_paths=[None, None], **kwargs):
         super().__init__(id=id, fastq_paths=fastq_paths, **kwargs)
+    
+    def add_mixin(self, mixin):
+        self.__class__ = type(
+            self.__class__.__name__,
+            (self.__class__, mixin),
+            {}
+        )
 
 
 S = TypeVar("S", bound=Sample)
-
-
 class Samples(UserList[S]):
     """A list of sample containers"""
 
@@ -78,44 +83,17 @@ class Samples(UserList[S]):
                 samples.append(Sample(id=id, **sample))
         return cls(samples)
 
-    def hydra_units_samples(self, *_, location: str = "samples", **kwargs):
-        """Write Hydra units and samples files"""
-        # Path(location).mkdir(parents=True, exist_ok=True)
-        # _units_path = Path(location) / "units.csv"
-        # _samples_path = Path(location) / "samples.csv"
-        # with (
-        #     open(_units_path, "w", encoding="utf-8") as units,
-        #     open(_samples_path, "w", encoding="utf-8") as samples,
-        # ):
-        #     pass
-
-        # FIXME: Implement hydra units and samples files
-        raise NotImplementedError
-
-    def nfcore_samplesheet(self, *_, location: str | Path, **kwargs) -> Path:
-        """Write a Nextflow samplesheet"""
-        Path(location).mkdir(parents=True, exist_ok=True)
-        _data = [
-            {
-                "sample": sample.id,
-                "fastq_1": str(sample.fastq_paths[0]),
-                "fastq_2": str(sample.fastq_paths[1]),
-                **{
-                    k: v[sample.id] if isinstance(v, Mapping) else v
-                    for k, v in kwargs.items()
-                },
-            }
-            for sample in self
-        ]
-
-        _header = ",".join(_data[0].keys())
-
-        _samplesheet = "\n".join([_header, *(",".join(d.values()) for d in _data)])
-        _path = Path(location) / "samples.nextflow.csv"
-        with open(_path, "w", encoding="utf-8") as handle:
-            handle.write(_samplesheet)
-
-        return _path
+    def add_mixin(self, mixin: type):
+        mixin_origin = get_origin(mixin) or mixin
+        (mixin_arg, *_) = get_args(mixin) or (None,)
+        self.__class__ = type(
+            self.__class__.__name__,
+            (self.__class__, mixin_origin),
+            {}
+        )
+        if mixin_arg:
+            for s in self:
+                s.add_mixin(mixin_arg)
 
     def __reduce__(self) -> Callable | tuple:
         return self.__class__, (self.data,)
