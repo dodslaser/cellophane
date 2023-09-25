3c6a82c3a2d57595903e7ee640a6c6ead96b5d3e
dodslaser <2969909+dodslaser@users.noreply.github.com>
2023-09-25 13:52:58 +0200

    refactor!: Simplify log management

--- cellophane/src/logs.py
+++ cellophane/src/logs.py
@@ -4,75 +4,47 @@ import atexit
 import logging
 from logging.handlers import QueueHandler, QueueListener
 from pathlib import Path
-from typing import Callable
-from queue import Queue
-from functools import wraps
+import multiprocessing as mp
 
 from rich.logging import RichHandler
 
 
-def get_log_queue(manager) -> Queue:
-    """Create a queue for logging and a listener to handle it."""
+def setup_logging():  # pragma: no cover
+    """Setup logging for multiprocessing."""
+    # Remove any existing handlers
+    root_logger = logging.getLogger()
+    root_logger.handlers = []
+
+    logger = logging.getLogger("cellophane")
+    logger.handlers = []
+
+    log_queue = mp.Manager().Queue(-1)
     console_handler = RichHandler(show_path=True)
     console_handler.setFormatter(
         logging.Formatter("%(label)s: %(message)s", datefmt="%H:%M:%S")
     )
-    queue = manager.Queue(-1)
-    listener = QueueListener(queue, console_handler)
+    listener = QueueListener(log_queue, console_handler)
     listener.start()
     atexit.register(listener.stop)
 
-    return queue
+    queue_handler = QueueHandler(log_queue)
+    logger.addHandler(queue_handler)
 
 
-def add_file_handler(logger, path: Path) -> None:
+def add_file_handler(logger: logging.LoggerAdapter, path: Path) -> None:
     """Add a file handler to a logger."""
     path.parent.mkdir(parents=True, exist_ok=True)
     file_handler = logging.FileHandler(path)
     file_handler.setFormatter(
         logging.Formatter("%(asctime)s : %(label)s : %(message)s")
     )
-    logger.addHandler(file_handler)
+    logger.logger.addHandler(file_handler)
 
 
-def get_logger(
+def get_labeled_adapter(
     label: str,
-    level: int,
-    queue: Queue,
-    path: Path | None = None,
+    logger: logging.Logger | None = None,
 ) -> logging.LoggerAdapter:
-    """Create a logger with a queue handler and a file handler if specified."""
-    logger = logging.getLogger(label)
-    logger.setLevel(level)
-    queue_handler = QueueHandler(queue)
-    logger.addHandler(queue_handler)
-
-    if path is not None:
-        add_file_handler(logger, path)
-
-    adapter = logging.LoggerAdapter(logger, {"label": label})
-
-    return adapter
-
-
-def log_exceptions(
-    logger: logging.LoggerAdapter,
-    exit: bool = True,
-    cleanup_fn: Callable | None = lambda: None,
-):
-    """Decorator to log exceptions."""
-
-    def wrapper(func):
-        @wraps(func)
-        def inner(*args, **kwargs):
-            try:
-                return func(*args, logger=logger, **kwargs)
-            except Exception as e:
-                logger.critical(f"Unhandled exception: {e}", exc_info=True)
-                cleanup_fn()
-                if exit:
-                    raise SystemExit(1)
-
-        return inner
-
-    return wrapper
+    if logger is None:
+        logger = logging.getLogger("cellophane")
+    return logging.LoggerAdapter(logger, {"label": label})
