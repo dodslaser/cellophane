1b9b2b6e9cfffb34f812898df0ba6757cd72c3cf
dodslaser <2969909+dodslaser@users.noreply.github.com>
2023-10-02 14:27:20 +0200

    fix: Ensure external paths are absolute

--- cellophane/src/testing/__init__.py
+++ cellophane/src/testing/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from shutil import copy, copytree
 
 from click.testing import CliRunner
-from pytest import LogCaptureFixture, fixture
+from pytest import FixtureRequest, LogCaptureFixture, fixture
 from ruamel.yaml import YAML
 
 import cellophane
@@ -16,7 +16,8 @@ _YAML = YAML(typ="safe", pure=True)
 def _create_structure(
     root: Path,
     structure: dict[str, str | dict[str, str]],
-    external: dict[Path] | None = None,
+    external_root: Path | None = None,
+    external: dict[str, str] | None = None,
 ):
     for path, content in structure.items():
         (root / "modules").mkdir(parents=True, exist_ok=True)
@@ -33,18 +34,22 @@ def _create_structure(
             (root / path).write_text(content)
 
     for src, dst in (external or {}).items():
-        if src.is_dir():
-            copytree(src, root / dst)
+        _src = Path(src)
+        if not _src.is_absolute():
+            _src = (external_root / src).resolve()
+        if _src.is_dir():
+            copytree(_src, root / dst)
         else:
-            copy(src, root / dst)
+            copy(_src, root / dst)
 
 
 @fixture
-def run_definition(tmp_path: Path, caplog: LogCaptureFixture):
+def run_definition(tmp_path: Path, caplog: LogCaptureFixture, request: FixtureRequest):
     """Run a cellophane wrapper from a definition YAML file."""
     # FIXME: Check output
     _runner = CliRunner()
     _handlers = logging.getLogger().handlers
+    _extenal_root = Path(request.fspath).parent
     logging.getLogger().handlers = []
 
     def inner(definition: Path):
@@ -54,9 +59,10 @@ def run_definition(tmp_path: Path, caplog: LogCaptureFixture):
         try:
             with _runner.isolated_filesystem(tmp_path) as td:
                 _create_structure(
-                    Path(td),
-                    _definition["structure"],
-                    _definition.get("external", None),
+                    root=Path(td),
+                    structure=_definition["structure"],
+                    external_root=_extenal_root,
+                    external=_definition.get("external", None),
                 )
                 _main = cellophane.cellophane("DUMMY", root=Path(td))
                 _runner.invoke(_main, _args)
