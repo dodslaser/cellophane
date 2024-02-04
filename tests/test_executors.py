47d9d03f0701d827c61036f77a4b7dffb61f6059
dodslaser <2969909+dodslaser@users.noreply.github.com>
2024-02-04 21:59:04 +0100

    tests: Add missing tests for executors

--- tests/test_executors.py
+++ tests/test_executors.py
@@ -1,38 +1,21 @@
 """Test cellphane.src.executors."""
 
+import logging
 import multiprocessing as mp
 import time
 from pathlib import Path
+from typing import Any
 from unittest.mock import MagicMock
 
 from mpire import WorkerPool
+from pytest import LogCaptureFixture, raises
+from pytest_mock import MockerFixture
 
-from cellophane import data, executors
+from cellophane import data, executors, logs
 
 
 class Test_SubprocessExecutor:
     """Test SubprocessExecutor."""
-    def test_executor(self, tmp_path: Path) -> None:
-        """Test SubprocessExecutor."""
-        config = data.Container(
-            workdir=tmp_path,
-            logdir=tmp_path,
-            executor={"cpus": 1, "memory": 1},
-        )
-        with WorkerPool(
-            daemon=False,
-            use_dill=True,
-        ) as pool:
-            spe = executors.SubprocesExecutor(
-                config=config,  # type: ignore[arg-type]
-                pool=pool,
-                log_queue=mp.Queue(),
-            )
-
-            result, _ = spe.submit("sleep .2", name="sleep", wait=True)
-
-            assert result.ready()
-            assert result.successful()
 
     def test_callback(self, tmp_path: Path) -> None:
         """Test callback."""
@@ -98,3 +81,71 @@ class Test_SubprocessExecutor:
             spe.terminate()
             spe.wait()
             assert all(r.ready() for r in results)
+
+    @staticmethod
+    def test_command_exception(
+        mocker: MockerFixture,
+        caplog: LogCaptureFixture,
+        tmp_path: Path,
+    ) -> None:
+        """Test command exception."""
+
+        mocker.patch(
+            "cellophane.executors.sp.Popen",
+            side_effect=Exception("DUMMY"),
+        )
+
+        config = data.Container(
+            workdir=tmp_path,
+            logdir=tmp_path,
+            executor={"cpus": 1, "memory": 1},
+        )
+
+        _log_queue = logs.start_queue_listener()
+
+        with (
+            WorkerPool(
+                daemon=False,
+                use_dill=True,
+            ) as pool,
+            caplog.at_level("DEBUG"),
+            raises(SystemExit) as exception,
+        ):
+            spe = executors.SubprocesExecutor(
+                config=config,  # type: ignore[arg-type]
+                pool=pool,
+                log_queue=_log_queue,
+            )
+
+            result = spe.submit("exit", name="exception")[0]
+
+            spe.wait()
+            result.get()
+
+        assert repr(exception.value) == "SystemExit(1)"
+        assert "Command failed with exception: Exception('DUMMY')" in caplog.messages
+
+    @staticmethod
+    def test_wait_for_uuid(tmp_path: Path) -> None:
+        """Test wait_for_uuid."""
+        with WorkerPool(
+            daemon=False,
+            use_dill=True,
+        ) as pool:
+            spe = executors.SubprocesExecutor(
+                config=data.Container(  # type: ignore[arg-type]
+                    workdir=tmp_path,
+                    logdir=tmp_path,
+                    executor={"cpus": 1, "memory": 1},
+                ),
+                pool=pool,
+                log_queue=mp.Queue(),
+            )
+            result1, uuid1 = spe.submit("sleep .1", name="sleep")
+            result2, _ = spe.submit("sleep 2", name="sleep")
+
+            spe.wait(uuid1)
+            spe.terminate()
+
+            assert result1.ready()
+            assert not result2.successful()
