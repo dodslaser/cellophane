c5c5313fc1ef57a3c53d7a251400baa89fc72236
dodslaser <2969909+dodslaser@users.noreply.github.com>
2024-02-05 16:15:39 +0100

    tests: Use a fixture for initializing the executor

--- tests/test_executors.py
+++ tests/test_executors.py
@@ -6,85 +6,85 @@ from pathlib import Path
 from unittest.mock import MagicMock
 
 from mpire import WorkerPool
-from pytest import LogCaptureFixture, raises
+from pytest import LogCaptureFixture, fixture, raises
 from pytest_mock import MockerFixture
 
 from cellophane import data, executors, logs
 
 
+@fixture(scope="function")
+def spe(tmp_path: Path) -> executors.SubprocesExecutor:
+    """Return a SubprocesExecutor."""
+    config = data.Container(
+        workdir=tmp_path,
+        logdir=tmp_path,
+        executor={"cpus": 1, "memory": 1},
+    )
+
+    log_queue = logs.start_queue_listener()
+
+    with WorkerPool(
+        daemon=False,
+        use_dill=True,
+    ) as pool:
+        yield executors.SubprocesExecutor(
+            config=config,  # type: ignore[arg-type]
+            pool=pool,
+            log_queue=log_queue,
+        )
+
+
 class Test_SubprocessExecutor:
     """Test SubprocessExecutor."""
 
-    def test_callback(self, tmp_path: Path) -> None:
+    @staticmethod
+    def test_callback(
+        spe: executors.SubprocesExecutor,  # pylint: disable=redefined-outer-name
+    ) -> None:
         """Test callback."""
-        config = data.Container(
-            workdir=tmp_path,
-            logdir=tmp_path,
-            executor={"cpus": 1, "memory": 1},
+        _callback = MagicMock()
+        _error_callback = MagicMock()
+        assert not _callback.called
+        assert not _error_callback.called
+        spe.submit(
+            "sleep 0",
+            name="sleep",
+            wait=True,
+            callback=_callback,
+            error_callback=_error_callback,
         )
-        with WorkerPool(
-            daemon=False,
-            use_dill=True,
-        ) as pool:
-            spe = executors.SubprocesExecutor(
-                config=config,  # type: ignore[arg-type]
-                pool=pool,
-                log_queue=mp.Queue(),
-            )
-            _callback = MagicMock()
-            _error_callback = MagicMock()
-            assert not _callback.called
-            assert not _error_callback.called
-            spe.submit(
-                "sleep 0",
-                name="sleep",
-                wait=True,
-                callback=_callback,
-                error_callback=_error_callback,
-            )
-            assert _callback.called
-            assert not _error_callback.called
-            spe.submit(
-                "exit 42",
-                name="exception",
-                wait=True,
-                callback=_callback,
-                error_callback=_error_callback,
-            )
-            assert _error_callback.called
+        assert _callback.called
+        assert not _error_callback.called
+        spe.submit(
+            "exit 42",
+            name="exception",
+            wait=True,
+            callback=_callback,
+            error_callback=_error_callback,
+        )
+        assert _error_callback.called
 
-    def test_executor_terminate_all(self, tmp_path: Path) -> None:
+    def test_executor_terminate_all(
+        self,
+        spe: executors.SubprocesExecutor,  # pylint: disable=redefined-outer-name
+    ) -> None:
         """Test that all processes are terminated when the executor is terminated."""
-        config = data.Container(
-            workdir=tmp_path,
-            logdir=tmp_path,
-            executor={"cpus": 1, "memory": 1},
-        )
-        with WorkerPool(
-            daemon=False,
-            use_dill=True,
-        ) as pool:
-            spe = executors.SubprocesExecutor(
-                config=config,  # type: ignore[arg-type]
-                pool=pool,
-                log_queue=mp.Queue(),
-            )
-            results = [
-                spe.submit("sleep 1", name="sleep")[0],
-                spe.submit("sleep 1", name="sleep")[0],
-                spe.submit("sleep 1", name="sleep")[0],
-            ]
-            time.sleep(0.1)
-            assert not any(r.ready() for r in results), [r.get() for r in results]
-            spe.terminate()
-            spe.wait()
-            assert all(r.ready() for r in results)
+        results = [
+            spe.submit("sleep 1", name="sleep")[0],
+            spe.submit("sleep 1", name="sleep")[0],
+            spe.submit("sleep 1", name="sleep")[0],
+        ]
+        time.sleep(0.1)
+        assert not any(r.ready() for r in results), [r.get() for r in results]
+        spe.terminate()
+        spe.wait()
+        assert all(r.ready() for r in results)
 
     @staticmethod
     def test_command_exception(
         mocker: MockerFixture,
         caplog: LogCaptureFixture,
-        tmp_path: Path,
+        spe: executors.SubprocesExecutor,  # pylint: disable=redefined-outer-name
     ) -> None:
         """Test command exception."""
 
@@ -93,28 +93,10 @@ class Test_SubprocessExecutor:
             side_effect=Exception("DUMMY"),
         )
 
-        config = data.Container(
-            workdir=tmp_path,
-            logdir=tmp_path,
-            executor={"cpus": 1, "memory": 1},
-        )
-
-        _log_queue = logs.start_queue_listener()
-
         with (
-            WorkerPool(
-                daemon=False,
-                use_dill=True,
-            ) as pool,
             caplog.at_level("DEBUG"),
             raises(SystemExit) as exception,
         ):
-            spe = executors.SubprocesExecutor(
-                config=config,  # type: ignore[arg-type]
-                pool=pool,
-                log_queue=_log_queue,
-            )
-
             result = spe.submit("exit", name="exception")[0]
 
             spe.wait()
@@ -124,26 +106,15 @@ class Test_SubprocessExecutor:
         assert "Command failed with exception: Exception('DUMMY')" in caplog.messages
 
     @staticmethod
-    def test_wait_for_uuid(tmp_path: Path) -> None:
+    def test_wait_for_uuid(
+        spe: executors.SubprocesExecutor,  # pylint: disable=redefined-outer-name
+    ) -> None:
         """Test wait_for_uuid."""
-        with WorkerPool(
-            daemon=False,
-            use_dill=True,
-        ) as pool:
-            spe = executors.SubprocesExecutor(
-                config=data.Container(  # type: ignore[arg-type]
-                    workdir=tmp_path,
-                    logdir=tmp_path,
-                    executor={"cpus": 1, "memory": 1},
-                ),
-                pool=pool,
-                log_queue=mp.Queue(),
-            )
-            result1, uuid1 = spe.submit("sleep .1", name="sleep")
-            result2, _ = spe.submit("sleep 2", name="sleep")
+        result1, uuid1 = spe.submit("sleep .1", name="sleep")
+        result2, _ = spe.submit("sleep 2", name="sleep")
 
-            spe.wait(uuid1)
-            spe.terminate()
+        spe.wait(uuid1)
+        spe.terminate()
 
-            assert result1.ready()
-            assert not result2.successful()
+        assert result1.ready()
+        assert not result2.successful()
