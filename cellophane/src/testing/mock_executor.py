6c6ed5db81e4e6d8bbc52083ff69fb690af23375
dodslaser <2969909+dodslaser@users.noreply.github.com>
2024-06-26 10:08:23 +0200

    refactor: Make mock executor more useful for testing and debug

--- cellophane/src/testing/mock_executor.py
+++ cellophane/src/testing/mock_executor.py
@@ -1,34 +1,40 @@
 from typing import Any, Callable
 from unittest.mock import MagicMock
-from uuid import uuid4
+from uuid import uuid4, UUID
 
 from attrs import define
 from mpire.async_result import AsyncResult
+from pathlib import Path
 
 from cellophane.src.executors.executor import Executor
+from cellophane.src.cfg import Config
+from logging import LoggerAdapter
+from time import sleep
 
 
 @define(slots=False, init=False)
 class MockExecutor(Executor, name="mock"):
-    def __attrs_post_init__(self, *args: Any, **kwargs: Any) -> None:
-        object.__setattr__(
-            self,
-            "submit",
-            MagicMock(
-                return_value=(MagicMock(), uuid4()),
-                side_effect=self.submit_,
-            ),
-        )
-
-    @staticmethod
-    def submit_(
-        *args: Any,
-        callback: Callable | None = None,
-        error_callback: Callable | None = None,
+    def target(
+        self,
+        *args: str,
+        name: str,
+        uuid: UUID,
+        workdir: Path,
+        env: dict[str, str],
+        os_env: bool,
+        cpus: int,
+        memory: int,
+        logger: LoggerAdapter,
         **kwargs: Any,
     ) -> None:
-        del args, kwargs
-
-        result_ = AsyncResult({}, callback, error_callback)
-        if callback:
-            callback(result_)
+        del kwargs
+        logger.debug(f"MockExecutor called with {name=}")
+        logger.debug(f"cmdline={' '.join(args)}")
+        logger.debug(f"{uuid=!s}")
+        logger.debug(f"{workdir=!s}")
+        for k, v in env.items():
+            logger.debug(f"env.{k}={v}")
+        logger.debug(f"{os_env=}")
+        logger.debug(f"{cpus=}")
+        logger.debug(f"{memory=}")
+        sleep(.1)  # Ensure logs are printed before the return
